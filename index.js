2c88782f35c10182b9cebffe4771f2f0706e7ad9
jaryal-anuj <jariyal.anuj@gmail.com>
2020-04-26 00:10:36 -0700

    starting the server

---
+++ index.js
@@ -0,0 +1,18 @@
+/**
+ * Primary file for th API
+ * 
+ * 
+ */
+
+
+ //Dependencies
+ var http = require('http');
+
+ //The server should respond to all requests with a string
+var server = http.createServer(function(req, res){
+    res.end("Hello world");
+});
+ // Start the server, and have it listen on port 3000
+ server.listen(3000, function(){
+    console.log("The server is listening on port 3000 now");
+ });
